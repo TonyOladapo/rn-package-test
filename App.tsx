0d7487d56ec59e09e36ee91ec1abface02587623
Tony <toneyobaze@gmail.com>
2021-07-05 15:25:08 +0000

    chore: create animated pressable component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Pressable from "./src/components/AnimatedPressable";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Pressable scaleValue={1.5}>
+        <View
+          style={{
+            padding: 32,
+            backgroundColor: "tomato",
+            borderRadius: 10,
+            height: 200,
+            width: 200,
+          }}
+        ></View>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
